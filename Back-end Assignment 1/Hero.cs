a954943eae397dc6be98eaf43e905e05198506de
Vilhelm Assersen <vilhelm.assersen@no.experis.com>
2023-01-12 13:45:48 +0100

    First commit

--- Back-end Assignment 1/Hero.cs
+++ Back-end Assignment 1/Hero.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Back_end_Assignment_1
 {
-    internal class Hero
+    class Hero
     {
+        private string heroClass;
+        private string name;
+        private string level;
+        private string levelAttributes;
+        private string equipment;
+        private string validWeaponTypes;
+        private string validArmorTypes;
+        public string HeroClass { get => heroClass; set => heroClass = value; }
+        public string Name { get => name; set => name = value; }
+        public string Level { get => level; set => level = value; }
+        public string LevelAttributes { get => levelAttributes; set => levelAttributes = value; }
+        public string Equipment { get => equipment; set => equipment = value; }
+        public string ValidWeaponTypes { get => validWeaponTypes; set => validWeaponTypes = value; }
+        public string ValidArmorTypes { get => validArmorTypes; set => validArmorTypes = value; }
+
     }
 }
